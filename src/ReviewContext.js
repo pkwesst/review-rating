c431c8a39a32220bada503e9ef86167c5ce539ac
pkwesst <pkwesst@gmail.com>
2022-06-02 19:36:02 +0900

    api

--- src/ReviewContext.js
+++ src/ReviewContext.js
@@ -11,15 +11,7 @@ export const ReviewStateContext = createContext();
 export const ReviewDispatchContext = createContext();
 
 const reducer = (state, action) => {
-  let newData = [
-    {
-      name: "책 이름2",
-      content: "리뷰 내용",
-      rating: 8,
-      created_date: new Date().getTime(),
-      id: 1,
-    },
-  ];
+  let newData = [];
   switch (action.type) {
     case "CREATE": {
       const created_date = new Date().getTime();
@@ -92,27 +84,27 @@ const ReviewContext = ({ children }) => {
   const dataId = useRef(1);
 
   // API 호출
-  // const getData = async () => {
-  //   setTimeout(async () => {
-  //     const res = await fetch(
-  //       "https://jsonplaceholder.typicode.com/comments"
-  //     ).then((res) => res.json());
-  //     const initData = res.slice(0, 5).map((it) => {
-  //       return {
-  //         name: it.name.substr(0, 15),
-  //         content: it.body,
-  //         rating: Math.floor(Math.random() * 10) + 1,
-  //         created_date: new Date().getTime(),
-  //         id: dataId.current++,
-  //       };
-  //     });
-  //     dispatch({ type: "READ", data: initData });
-  //   }, 1000);
-  // };
+  const getData = async () => {
+    setTimeout(async () => {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/comments"
+      ).then((res) => res.json());
+      const initData = res.slice(0, 3).map((it) => {
+        return {
+          name: it.name.substr(0, 15),
+          content: it.body,
+          rating: Math.floor(Math.random() * 10) + 1,
+          created_date: new Date().getTime(),
+          id: dataId.current++,
+        };
+      });
+      dispatch({ type: "READ", data: initData });
+    }, 1000);
+  };
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
   useEffect(() => {
     const localData = localStorage.getItem("review");
